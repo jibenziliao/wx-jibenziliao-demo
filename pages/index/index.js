c6353e57cbd1b7c4cc7d88910e65a20d3e400e1a
jibenziliao <1501718947@qq.com>
2017-08-11 14:06:25 +0800

    Init project.

---
+++ pages/index/index.js
@@ -0,0 +1,26 @@
+//index.js
+//获取应用实例
+const app = getApp()
+Page({
+  data: {
+    motto: '你好，世界！',
+    userInfo: {}
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    let that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  }
+})
